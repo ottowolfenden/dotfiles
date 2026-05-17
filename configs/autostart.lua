ee058a9f1bd30b3a111b58a10078a3db0587ff50
Otto Wolfenden <ottowolfenden@gmail.com>
2026-05-17 23:44:04 +0100

    add global copy and paste reformat binds.lua add clipse to autostart

--- configs/autostart.lua
+++ configs/autostart.lua
@@ -1,5 +1,6 @@
 hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("swaync")
+    hl.exec_cmd("clipse -listen")
     -- hl.exec_cmd("waybar")
 end)
--- configs/binds.lua
+++ configs/binds.lua
@@ -29,17 +29,40 @@ hl.bind("SUPER + V", hl.dsp.exec_cmd("pkill clipse || kitty --class clipse -e cl
 hl.bind("SUPER + equal", hl.dsp.exec_cmd("pkill qalc || kitty qalc", { float = true }))
 hl.bind("XF86Calculator", hl.dsp.exec_cmd("pkill qalc || kitty qalc", { float = true }))
 
+-- global shortcuts
+local active_class = ""
+hl.on("window.active", function(w) active_class = w.class end)
+
+local keys = { "C", "V" }
+for _, key in ipairs(keys) do
+    hl.bind("SUPER + ALT + " .. key, function()
+        local target = {
+            mods = (active_class == "kitty") and "CTRL + SHIFT" or "CTRL",
+            key = key,
+            window = "activewindow"
+        }
+        target.state = "down"
+        hl.dispatch(hl.dsp.send_key_state(target))
+        target.state = "up"
+        hl.dispatch(hl.dsp.send_key_state(target))
+    end)
+end
+
 -- fn keys
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+hl.bind("XF86AudioRaiseVolume",
+    hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
     { locked = true, repeating = true }
 )
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+hl.bind("XF86AudioLowerVolume",
+    hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
     { locked = true, repeating = true }
 )
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+hl.bind("XF86AudioMute",
+    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
     { locked = true, repeating = true }
 )
-hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+hl.bind("XF86AudioMicMute",
+    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
     { locked = true, repeating = true }
 )
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
@@ -48,7 +71,7 @@ hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
-hl.bind("SUPER+ space",
+hl.bind("SUPER + space",
     hl.dsp.exec_cmd(
         "BC='platform::kbd_backlight'; [ $(brightnessctl --device=$BC get) -eq 0 ] && brightnessctl --device=$BC set 100% || brightnessctl --device=$BC set 0%"
     ),
