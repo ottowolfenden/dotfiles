4306a95bc5836693b51d5ae518129bdc4b75fd29
Otto Wolfenden <ottowolfenden@gmail.com>
2026-05-29 21:08:12 +0100

    change screenshot script to save the filename as a hash of the date rather than the actual date, and fix the slurp black border appearing in screenshots

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -4,5 +4,12 @@ if pgrep -x "slurp" > /dev/null || pgrep -x "grim" > /dev/null; then
     pkill -x "slurp"
     pkill -x "grim"
 else
-    grim -g "$(slurp)" - | tee ~/screenshots/"$(date +'%Y-%m-%d %H:%M:%S').png" | wl-copy
+    while true; do
+        FILENAME=$(date +'%Y-%m-%d-%H-%M-%S-%N' | md5sum | cut -c 1-5)
+        if [ ! -f ~/screenshots/"${FILENAME}.png" ]; then
+            break
+        fi
+        sleep 0.001
+    done
+    grim -g "$(slurp -w 0)" - | tee ~/screenshots/"${FILENAME}.png" | wl-copy
 fi
