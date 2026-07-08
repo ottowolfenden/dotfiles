dfc07f7d19e55953bd705e6702372e156e06aaca
Otto Wolfenden <ottowolfenden@gmail.com>
2026-07-08 16:43:23 +0100

    add keybind for fullscreen screenshot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+mode=$1
+
 if pgrep -x "slurp" > /dev/null || pgrep -x "grim" > /dev/null; then
     pkill -x "slurp"
     pkill -x "grim"
 else
     while true; do
-        FILENAME=$(date +'%Y-%m-%d-%H-%M-%S-%N' | md5sum | cut -c 1-5)
-        if [ ! -f ~/screenshots/"${FILENAME}.png" ]; then
+        filename=$(date +'%Y-%m-%d-%H-%M-%S-%N' | md5sum | cut -c 1-5)
+        if [ ! -f ~/screenshots/"${filename}.png" ]; then
             break
         fi
         sleep 0.001
     done
-    SLURP=$(slurp)
-    sleep 0.2
-    grim -g "$SLURP" - | tee ~/screenshots/"${FILENAME}.png" | wl-copy
+    if [[ $mode == "region" ]]; then
+        slurp=$(slurp)
+        sleep 0.2
+        grim -g "$slurp" - | tee ~/screenshots/"${filename}.png" | wl-copy
+    else
+        grim - | tee ~/screenshots/"${filename}.png" | wl-copy
+    fi
 fi
