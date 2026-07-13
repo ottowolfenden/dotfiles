eaed59a874a0251af5d82060d230250445c78ecf
Otto Wolfenden <ottowolfenden@gmail.com>
2026-07-13 17:03:24 +0100

    write script to search for files and directories

---
+++ scripts/find-fsentries.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+dir="$1"
+pattern="$2"
+type=$3
+opt=$4
+
+if [[ $type == -f ]]; then
+    if [[ $opt == --includehidden ]]; then
+        find "$dir" -type f -iname "$pattern" -print0 2>/dev/null
+    else
+        find "$dir" -name ".*" -prune -o \
+            -type f -iname "$pattern" -print0 2>/dev/null
+    fi
+else
+    if [[ $opt == --includehidden ]]; then
+        find "$dir" -type d -iname "$pattern" -print0 2>/dev/null
+    else
+        find "$dir" -name ".*" -prune -o \
+            -type d -iname "$pattern" -print0 2>/dev/null
+    fi
+fi | while IFS= read -r -d '' path; do
+        numfiles=$(find "$path" -type f -mindepth 1 | wc -l)
+        numsubdirs=$(find "$path" -maxdepth 1 -type d -mindepth 1 | wc -l)
+        stats=$(stat -c "%W %Y %X %s" "$path")
+        isrootowned=$( [[ $(stat -c "%u" "$path") -eq 0 ]] && echo true || echo false)
+
+        printf '%s\t%s %s %s %s\n' "$path" "$stats" "$isrootowned" "$numfiles" "$numsubdirs"
+done | jq -R '
+  split("\t") as $parts
+  | $parts[1] | split(" ") as $stats
+  | {
+      path: $parts[0],
+      created: $stats[0] | tonumber,
+      modified: $stats[1] | tonumber,
+      accessed: $stats[2] | tonumber,
+      byteSize: $stats[3] | tonumber,
+      isRootOwned: $stats[4] | toboolean,
+      numFiles: $stats[5] | tonumber,
+      numSubdirs: $stats[6] | tonumber
+    }
+' | jq -s '.'
