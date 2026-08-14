bf07cffb7585ee25724b2d82dd4b145724540460
Otto Wolfenden <ottowolfenden@gmail.com>
2026-08-14 11:03:01 +0100

    toggle keyboard backlight with light/dark mode

--- scripts/keyboard-backlight.sh
+++ scripts/keyboard-backlight.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 BC='platform::kbd_backlight';
-[ $(brightnessctl --device=$BC get) -eq 0 ] && brightnessctl --device=$BC set 25% || brightnessctl --device=$BC set 0%
+current=$(brightnessctl --device=$BC get)
+
+if [[ $1 == "on" || $current == 0 ]]; then
+    brightnessctl --device=$BC set 25%
+elif [[ $1 == "off" || $current -gt 0 ]]; then
+    brightnessctl --device=$BC set 0%
+fi
--- scripts/set-theme.sh
+++ scripts/set-theme.sh
@@ -2,20 +2,22 @@
 
 mode=$1
 
-if [ $mode = "dark" ]; then
+if [[ $mode == "dark" ]]; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
     sed -i 's/gtk-theme-name=.*/gtk-theme-name=Adwaita-dark/' ~/.config/gtk-3.0/settings.ini
     sed -i 's/gtk-application-prefer-dark-theme=.*/gtk-application-prefer-dark-theme=true/' ~/.config/gtk-3.0/settings.ini
     sed -i "s/color_scheme_path=.*/color_scheme_path=\/usr\/share\/qt6ct\/colors\/darker.conf/" ~/.config/qt6ct/qt6ct.conf
-    awww img $("$HOME"/dotfiles/scripts/daily-wallpaper.sh dark) --transition-type none
-elif [ $mode = "light" ]; then
+    awww img $(~/dotfiles/scripts/daily-wallpaper.sh dark) --transition-type none
+    ~/dotfiles/scripts/keyboard-backlight.sh on
+elif [[ $mode == "light" ]]; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     sed -i 's/gtk-theme-name=.*/gtk-theme-name=Adwaita/' ~/.config/gtk-3.0/settings.ini
     sed -i 's/gtk-application-prefer-dark-theme=.*/gtk-application-prefer-dark-theme=false/' ~/.config/gtk-3.0/settings.ini
     sed -i "s/color_scheme_path=.*/color_scheme_path=\/usr\/share\/qt6ct\/colors\/simple.conf/" ~/.config/qt6ct/qt6ct.conf
-    awww img $("$HOME"/dotfiles/scripts/daily-wallpaper.sh light) --transition-type none
+    awww img $(~/dotfiles/scripts/daily-wallpaper.sh light) --transition-type none
+    ~/dotfiles/scripts/keyboard-backlight.sh off
 else
     exit 1
 fi
